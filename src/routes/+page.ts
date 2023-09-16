2f44675f2c993ee03b794385ba0eaf6de48411b4
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-09-15 18:57:01 -0700

    feat: update example to use actual sanity project images

---
+++ src/routes/+page.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@sanity/client';
+import groq from 'groq';
+
+const client = createClient({
+	projectId: '4kvint4g',
+	dataset: 'production',
+	useCdn: true,
+	apiVersion: '2023-05-19'
+});
+
+export async function load() {
+	const images = await client.fetch(groq`
+  *[_type  == 'site'][0]{
+    'images': overview->images,
+  }
+`);
+
+	return images;
+}
