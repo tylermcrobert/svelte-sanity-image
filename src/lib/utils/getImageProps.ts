587765d2291d12def9b6d82b2aef70b862f0672b
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-09-16 19:50:38 -0700

    refactor: include getSrcSet in getImageProps

--- src/lib/utils/getImageProps.ts
+++ src/lib/utils/getImageProps.ts
@@ -1,7 +1,14 @@
 import getImageDimensions from './getImageDimensions';
-import getSrcset from './getSrcset';
-import type { GetImagePropsReturn, GetImagePropsOptions } from './types';
+import { DEFAULT_QUALITY, IMG_DEVICE_SIZES, IMG_SCALING } from './constants';
 import getBuilder from './getBuilder';
+
+import type { GetImagePropsReturn, GetImagePropsOptions } from './types';
+
+/**
+ * Return an object with image props
+ * @param param0
+ * @returns GetImagePropsReturn
+ */
 
 export default function getImageProps({
 	image,
@@ -11,10 +18,36 @@ export default function getImageProps({
 }: GetImagePropsOptions): GetImagePropsReturn {
 	const { width, height } = getImageDimensions(image);
 
+	/**
+	 * Get a srcset string for responsive images.
+	 */
+
+	function getSrcset() {
+		return IMG_DEVICE_SIZES.map(getUrlByWidth).join(', ');
+	}
+
+	/**
+	 * Gets a url by it's width
+	 * @param width width of srcset size
+	 * @returns a return
+	 */
+
+	function getUrlByWidth(width: number) {
+		let builder = getBuilder(image, client)
+			.width(width)
+			.auto('format')
+			.quality(quality || DEFAULT_QUALITY);
+
+		if (enforcedAspect) {
+			builder = builder.height(Math.round(width / enforcedAspect));
+		}
+
+		return `${builder.url()} ${Math.round(width / IMG_SCALING)}w`;
+	}
+
 	return {
 		src: getBuilder(image, client).url(),
-		srcset: getSrcset(image, client, { quality, enforcedAspect, client }),
-
+		srcset: getSrcset(),
 		width,
 		height: enforcedAspect ? Math.round(width / enforcedAspect) : height
 	};
