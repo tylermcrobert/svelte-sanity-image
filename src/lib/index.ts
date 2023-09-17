5d9a994602a24c2be99fff5479f2eb8dd9fd20f8
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2023-09-17 15:35:25 -0700

    feat: expose sanity Image Source

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,8 @@
 import SanityImage from './SanityImage.svelte';
+import getImageDimensions from './utils/getImageDimensions';
 
 export type { SanityImageSource } from './utils/types';
+
+export { getImageDimensions };
 
 export default SanityImage;
