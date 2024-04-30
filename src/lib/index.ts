a7806ea888e26b1ad81d75a0174259ef2d916929
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-30 16:36:47 -0700

    refactor: ImageSource instead of ImageObject

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,11 +2,7 @@ import Image from './SvelteSanityImage.svelte';
 import getImageDimensions from './utils/getImageDimensions';
 import imageUrlBuilder from '@sanity/image-url';
 
-export type {
-	SanityImageObject,
-	SanityImageSource,
-	Props
-} from './utils/types';
+export type { SanityImageSource, Props } from './utils/types';
 export { getImageDimensions, Image, imageUrlBuilder };
 
 export default Image;
