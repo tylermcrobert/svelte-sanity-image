ba032e30e1cba096ad6b6dac020eb067e0794634
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-29 21:18:53 -0700

    chore: export type

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,7 +2,11 @@ import Image from './SvelteSanityImage.svelte';
 import getImageDimensions from './utils/getImageDimensions';
 import imageUrlBuilder from '@sanity/image-url';
 
-export type { SanityImageObject, Props } from './utils/types';
+export type {
+	SanityImageObject,
+	SanityImageSource,
+	Props
+} from './utils/types';
 export { getImageDimensions, Image, imageUrlBuilder };
 
 export default Image;
