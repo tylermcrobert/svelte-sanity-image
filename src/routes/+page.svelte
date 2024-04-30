66f5b66e6f3d2ff1d48dc6cae9f0bce9bd574d4e
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-30 16:57:19 -0700

    feat: add more image queries

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,29 @@
 	export let data;
 </script>
 
+<hr />
+
+<div>Raw:</div>
 <div class="images">
-	{#each data.images as { image, alt }}
+	{#each data.raw as { image, alt }}
+		<Image {image} {client} {alt} sizes="100px" />
+	{/each}
+</div>
+
+<hr />
+
+<div>Refs:</div>
+<div class="images">
+	{#each data.refs as { image, alt }}
+		<Image {image} {client} {alt} sizes="100px" />
+	{/each}
+</div>
+
+<hr />
+
+<div>Expanded:</div>
+<div class="images">
+	{#each data.extended as { image, alt }}
 		<Image {image} {client} {alt} sizes="100px" />
 	{/each}
 </div>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,11 +1,40 @@
-import type { SanityImageObject } from '$lib';
+import type { SanityImageSource } from '$lib';
+import type { SanityAsset } from '@sanity/image-url/lib/types/types';
 import { client } from '../sanity';
 
-const imagesQuery = `*[_type == 'post'][0].images`;
+const imagesQuery = `*[_type == 'post'][0]{
+	"raw": images[]{
+		image,
+		alt
+	},
+	"refs": images[]{
+		"image": image.asset,
+		alt
+	},
+	"extended": images[]{
+		"image":image.asset->
+	}
+}`;
 
-export type ImagesQuery = { image: SanityImageObject; alt: string }[];
+export type ImagesQuery = {
+	raw: {
+		image: SanityImageSource;
+		alt: string;
+	}[];
+	refs: {
+		image: {
+			_ref: string;
+			_type: 'reference';
+		};
+		alt: string;
+	}[];
+	extended: {
+		image: SanityAsset;
+		alt: string;
+	}[];
+};
 
 export async function load() {
 	const images: ImagesQuery = await client.fetch(imagesQuery);
-	return { images };
+	return images;
 }
