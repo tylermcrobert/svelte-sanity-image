96a2f793eee68805c56c5ea0630686cd5963bc29
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-29 19:51:52 -0700

    refactor: test helper

--- src/lib/utils/getImageProps.test.ts
+++ src/lib/utils/getImageProps.test.ts
@@ -3,6 +3,10 @@ import getImageProps from './getImageProps';
 import { client } from '../../sanity';
 import { BASE_URL, DEFAULT_IMAGE } from '$lib/tests/constants';
 
+function flattenStr(str: string) {
+	return str.replace(/\s+/g, ' ').trim();
+}
+
 describe('getImageProps', () => {
 	const image = DEFAULT_IMAGE;
 
@@ -12,7 +16,7 @@ describe('getImageProps', () => {
 		expect(props.src).toBe(BASE_URL);
 
 		expect(props.srcset).toBe(
-			`
+			flattenStr(`
 			${BASE_URL}?w=640 640w, 
 			${BASE_URL}?w=750 750w, 
 			${BASE_URL}?w=828 828w, 
@@ -20,9 +24,7 @@ describe('getImageProps', () => {
 			${BASE_URL}?w=1200 1200w, 
 			${BASE_URL}?w=1920 1920w, 
 			${BASE_URL}?w=2048 2048w, 
-			${BASE_URL}?w=3840 3840w`
-				.replace(/\s+/g, ' ')
-				.trim()
+			${BASE_URL}?w=3840 3840w`)
 		);
 
 		expect(props.width).toBe(8025);
@@ -35,7 +37,7 @@ describe('getImageProps', () => {
 		expect(props.src).toBe(`${BASE_URL}?q=50`);
 
 		expect(props.srcset).toBe(
-			`
+			flattenStr(`
 			${BASE_URL}?w=640&q=50 640w, 
 			${BASE_URL}?w=750&q=50 750w, 
 			${BASE_URL}?w=828&q=50 828w, 
@@ -43,9 +45,7 @@ describe('getImageProps', () => {
 			${BASE_URL}?w=1200&q=50 1200w, 
 			${BASE_URL}?w=1920&q=50 1920w, 
 			${BASE_URL}?w=2048&q=50 2048w, 
-			${BASE_URL}?w=3840&q=50 3840w`
-				.replace(/\s+/g, ' ')
-				.trim()
+			${BASE_URL}?w=3840&q=50 3840w`)
 		);
 
 		expect(props.width).toBe(8025);
