45df17630cbd82d5c5659d79038eee96f70933a2
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-04-30 17:29:16 -0700

    refactor: add referenceId to dims folder

--- src/lib/utils/getImageDimensions.ts
+++ src/lib/utils/getImageDimensions.ts
@@ -1,5 +1,9 @@
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types.d.ts';
-import { getReferenceId } from './getReferenceId';
+import type {
+	SanityImageSource,
+	SanityImageObject,
+	SanityReference,
+	SanityAsset
+} from '@sanity/image-url/lib/types/types.d.ts';
 
 /**
  * Output of getImageDimensions()
@@ -10,6 +14,42 @@ export type ImageDimensionsOutput = {
 	height: number;
 	aspectRatio: number;
 };
+
+/**
+ * Retrieves the reference ID of a Sanity image.
+ * @param image - The Sanity image source.
+ * @returns The reference ID of the image, or undefined if not found.
+ */
+
+// TODO:
+// Add tests
+
+// REFERENCE
+// https://github.com/lorenzodejong/next-sanity-image/blob/3b11583385d69a3e195aa142c974d2e014d492c6/src/useNextSanityImage.ts#L33
+
+// TODO: catch this:
+
+// interface SanityImageWithAssetStub {
+// 	asset: {
+// 		url: string;
+// 	};
+// }
+
+export function getReferenceId(image: SanityImageSource): string | undefined {
+	if (typeof image === 'string') {
+		return image;
+	}
+
+	const obj = image as SanityImageObject;
+	const ref = image as SanityReference;
+	const img = image as SanityAsset;
+
+	if (obj.asset) {
+		return obj.asset._ref || (obj.asset as SanityAsset)._id;
+	}
+
+	return ref._ref || img._id || undefined;
+}
 
 /**
  * Takes sanity image and pulls
