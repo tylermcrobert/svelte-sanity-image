c6494562d5540239c09671d65fc3e7471f513b20
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-12-08 22:08:43 -0800

    chore: remove prev

--- previous/src/sanity.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const client: import("@sanity/client").SanityClient;
--- previous/src/sanity.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client';
-
-export const client = createClient({
-	projectId: '7ehtrhwv',
-	dataset: 'production',
-	useCdn: true,
-	apiVersion: '2023-05-19'
-});
