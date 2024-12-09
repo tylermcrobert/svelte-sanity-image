d5b50c2f190ff0968c6dc1e916c274aa719b749a
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-12-09 12:51:04 -0800

    feat: demo updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,19 @@
 	import SvelteSanityImage from '../lib/SvelteSanityImage.svelte';
 
 	let { data } = $props();
+
+	const TEST_IMAGE = { _ref: 'image-baa6daf0bffc612ad17f5fdd9dd1d0f2ad2f5059-1200x800-png' };
 </script>
+
+<div class="wrapper">
+	<SvelteSanityImage {client} image={TEST_IMAGE} sizes="25vw" />
+	<SvelteSanityImage {client} image={TEST_IMAGE} aspect={0.75} sizes="25vw" />
+	<SvelteSanityImage {client} image={TEST_IMAGE} aspect={1} sizes="25vw" />
+	<SvelteSanityImage {client} image={TEST_IMAGE} aspect={1.25} sizes="25vw" />
+	<SvelteSanityImage {client} image={TEST_IMAGE} height={1000} sizes="25vw" />
+</div>
+
+<hr style="margin: 100px 0" />
 
 <div class="wrapper">
 	{#each data.images as { image, alt }}
@@ -16,6 +28,7 @@
 		display: grid;
 		grid-template-columns: repeat(4, 1fr);
 		align-items: flex-start;
+		gap: 8px;
 	}
 
 	.wrapper :global(img) {
