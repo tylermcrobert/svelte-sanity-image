b2e220d806c4cf039c45b18b267a282eb10c1a47
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-12-08 17:48:02 -0800

    feat: extend builder

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,6 +7,34 @@
  *
  * https://nextjs.org/docs/pages/api-reference/components/image#imagesizes
  */
-export const DEFAULT_IMAGE_SIZES = [
-	640, 750, 828, 1080, 1200, 1920, 2048, 3840
-];
+export const DEFAULT_IMAGE_SIZES = [640, 750, 828, 1080, 1200, 1920, 2048, 3840];
+
+export const VALID_BUILDER_OPTIONS = new Set([
+	// 'blur',
+	// 'bg',
+	// 'dpr',
+	'width',
+	'height'
+	// 'focalPoint',
+	// 'maxWidth',
+	// 'maxHeight',
+	// 'minWidth',
+	// 'minHeight',
+	// 'sharpen',
+	// 'rect',
+	// 'format',
+	// 'invert',
+	// 'orientation',
+	// 'quality',
+	// 'download',
+	// 'flipHorizontal',
+	// 'flipVertical',
+	// 'ignoreImageParams',
+	// 'fit',
+	// 'crop',
+	// 'saturation',
+	// 'auto',
+	// 'pad',
+	// 'vanityName',
+	// 'frame'
+] as const);
