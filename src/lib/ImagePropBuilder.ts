a24b86b93bbb6f19b9da61fcf4ac7b4aa70c1698
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-12-10 10:54:05 -0800

    refactor: simplify

--- src/lib/ImagePropBuilder.ts
+++ src/lib/ImagePropBuilder.ts
@@ -167,9 +167,7 @@ export class ImagePropBuilder {
 			.map((breakpoint) => {
 				// Calculate height based on aspect ratio if specified
 				if (this.customDimensions.aspectRatio || this.customDimensions.height) {
-					const calculatedHeight = Math.round(
-						breakpoint / (this.customDimensions.aspectRatio || this.dimensions.aspectRatio)
-					);
+					const calculatedHeight = Math.round(breakpoint / this.dimensions.aspectRatio);
 
 					return this.urlBuilder.height(calculatedHeight).width(breakpoint).url();
 				}
