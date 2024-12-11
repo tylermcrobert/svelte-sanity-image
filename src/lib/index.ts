f0a25900a6da44304f8637983836d89382d329b2
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-12-11 14:24:48 -0800

    chore: export SanityImageSource

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,6 @@ export { default as imageUrlBuilder } from '@sanity/image-url';
 
 import Image from './SvelteSanityImage.svelte';
 
-export type { SvelteSanityImageProps } from './types.js';
+export type { SvelteSanityImageProps, SanityImageSource } from './types.js';
 
 export default Image;
