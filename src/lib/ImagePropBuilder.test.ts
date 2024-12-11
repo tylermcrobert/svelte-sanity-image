cdcc76138d4e3585d42337a2c4ff68c0d3f713a0
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2024-12-10 17:30:05 -0800

    chore: tests

---
+++ src/lib/ImagePropBuilder.test.ts
@@ -0,0 +1,141 @@
+import { describe, it, expect } from 'vitest';
+import { client } from '../sanity.js';
+import { BASE_URL, DEFAULT_IMAGE } from '../constants.js';
+import { ImagePropBuilder } from './ImagePropBuilder.js';
+
+function flattenStr(str: string) {
+	return str.replace(/\s+/g, ' ').trim();
+}
+
+describe('getImageProps', () => {
+	it('Correctly renders with base implementation', () => {
+		const props = new ImagePropBuilder(DEFAULT_IMAGE, client);
+
+		expect(props.src).toBe(BASE_URL);
+		expect(props.dimensions.width).toBe(2400);
+		expect(props.dimensions.height).toBe(1600);
+		expect(props.srcset).toBe(
+			flattenStr(`
+					${BASE_URL}?w=640 640w, 
+					${BASE_URL}?w=750 750w, 
+					${BASE_URL}?w=828 828w, 
+					${BASE_URL}?w=1080 1080w, 
+					${BASE_URL}?w=1200 1200w, 
+					${BASE_URL}?w=1920 1920w, 
+					${BASE_URL}?w=2048 2048w
+				`)
+		);
+	});
+
+	it('Correctly renders with quality prop', () => {
+		const props = new ImagePropBuilder(DEFAULT_IMAGE, client, { quality: 50 });
+
+		expect(props.src).toBe(`${BASE_URL}?q=50`);
+		expect(props.dimensions.width).toBe(2400);
+		expect(props.dimensions.height).toBe(1600);
+		expect(props.srcset).toBe(
+			flattenStr(`
+					${BASE_URL}?w=640&q=50 640w, 
+					${BASE_URL}?w=750&q=50 750w, 
+					${BASE_URL}?w=828&q=50 828w, 
+					${BASE_URL}?w=1080&q=50 1080w, 
+					${BASE_URL}?w=1200&q=50 1200w, 
+					${BASE_URL}?w=1920&q=50 1920w, 
+					${BASE_URL}?w=2048&q=50 2048w
+				`)
+		);
+	});
+
+	describe('Aspect Ratios', () => {
+		it('Correctly renders portrait aspect', () => {
+			const props = new ImagePropBuilder(DEFAULT_IMAGE, client, { aspect: 0.75 });
+
+			expect(props.src).toBe(`${BASE_URL}?rect=600,0,1200,1600&w=1200&h=1600`);
+			expect(props.dimensions.width).toBe(1200);
+			expect(props.dimensions.height).toBe(1600);
+			expect(props.srcset).toBe(
+				flattenStr(`
+					${BASE_URL}?rect=600,0,1200,1600&w=640&h=853 640w, 
+					${BASE_URL}?rect=600,0,1200,1600&w=750&h=1000 750w, 
+					${BASE_URL}?rect=600,0,1200,1600&w=828&h=1104 828w, 
+					${BASE_URL}?rect=600,0,1200,1600&w=1080&h=1440 1080w, 
+					${BASE_URL}?rect=600,0,1200,1600&w=1200&h=1600 1200w
+				`)
+			);
+		});
+
+		it('Correctly renders landscape aspect', () => {
+			const props = new ImagePropBuilder(DEFAULT_IMAGE, client, { aspect: 1.25 });
+
+			expect(props.src).toBe(`${BASE_URL}?rect=200,0,2000,1600&w=2000&h=1600`);
+			expect(props.dimensions.width).toBe(2000);
+			expect(props.dimensions.height).toBe(1600);
+			expect(props.srcset).toBe(
+				flattenStr(`
+						${BASE_URL}?rect=200,0,2000,1600&w=640&h=512 640w,
+						${BASE_URL}?rect=200,0,2000,1600&w=750&h=600 750w, 
+						${BASE_URL}?rect=200,0,2001,1600&w=828&h=662 828w, 
+						${BASE_URL}?rect=200,0,2000,1600&w=1080&h=864 1080w, 
+						${BASE_URL}?rect=200,0,2000,1600&w=1200&h=960 1200w, 
+						${BASE_URL}?rect=200,0,2000,1600&w=1920&h=1536 1920w
+					`)
+			);
+		});
+
+		it('Correctly renders square aspect', () => {
+			const props = new ImagePropBuilder(DEFAULT_IMAGE, client, { aspect: 1 });
+
+			expect(props.src).toBe(`${BASE_URL}?rect=400,0,1600,1600&w=1600&h=1600`);
+			expect(props.dimensions.width).toBe(1600);
+			expect(props.dimensions.height).toBe(1600);
+			expect(props.srcset).toBe(
+				flattenStr(`
+						${BASE_URL}?rect=400,0,1600,1600&w=640&h=640 640w,
+						${BASE_URL}?rect=400,0,1600,1600&w=750&h=750 750w, 
+						${BASE_URL}?rect=400,0,1600,1600&w=828&h=828 828w, 
+						${BASE_URL}?rect=400,0,1600,1600&w=1080&h=1080 1080w, 
+						${BASE_URL}?rect=400,0,1600,1600&w=1200&h=1200 1200w`)
+			);
+		});
+
+		it('Correctly renders Aspect + Width', () => {
+			const props = new ImagePropBuilder(DEFAULT_IMAGE, client, { aspect: 1.5, width: 800 });
+
+			expect(props.dimensions.width).toBe(800);
+			expect(props.dimensions.height).toBe(533);
+			expect(props.src).toBe(`${BASE_URL}?rect=0,1,2400,1599&w=800&h=533`);
+			expect(props.srcset).toBe(
+				flattenStr(`
+					${BASE_URL}?rect=0,1,2400,1598&w=640&h=426 640w, 
+					${BASE_URL}?w=750&h=500 750w
+			`)
+			);
+		});
+
+		it('Correctly renders Aspect + Height', () => {
+			const props = new ImagePropBuilder(DEFAULT_IMAGE, client, { aspect: 1.5, height: 600 });
+
+			expect(props.dimensions.width).toBe(900);
+			expect(props.dimensions.height).toBe(600);
+			expect(props.src).toBe(`${BASE_URL}?w=900&h=600`);
+			expect(props.srcset).toBe(
+				flattenStr(`
+					${BASE_URL}?rect=1,0,2398,1600&w=640&h=427 640w, 
+					${BASE_URL}?w=750&h=500 750w, 
+					${BASE_URL}?w=828&h=552 828w
+			`)
+			);
+		});
+	});
+
+	it('Throws error when user defines width, height, aspect', () => {
+		expect(
+			() =>
+				new ImagePropBuilder(DEFAULT_IMAGE, client, {
+					aspect: 1.5,
+					height: 400,
+					width: 400
+				})
+		).toThrowError('Cannot define width, height, and aspect');
+	});
+});
