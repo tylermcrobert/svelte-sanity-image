e56eac0e3db874f13a0a254b2b59180721c52195
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2025-05-10 16:10:16 -0700

    feat: expose getImageDimensions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 export { default as imageUrlBuilder } from '@sanity/image-url';
 export { getImageProps } from './getImageProps.js';
+export { getImageDimensions } from './getImageDimensions.js';
 import Image from './SvelteSanityImage.svelte';
 
 export type { SvelteSanityImageProps, SanityImageSource } from './types.js';
